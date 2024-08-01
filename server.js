e2ba0869bd9a0f7695d647f5f588bdba44633b0a
MaheshXenia <mahesh.xenia@gmail.com>
2024-08-01 18:46:39 +0530

    commit 1st cart

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const categoryRoutes = require('./routes/categoryRoutes');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,6 +25,7 @@ app.use(cors())
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/cart', cartRoutes);
 
 
 
